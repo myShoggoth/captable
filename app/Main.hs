1a9ee5e7d9bb0514768efd4a50a614fdda039000
Andrew Boardman <andrew@haskell.foundation>
2021-10-19 14:23:33 -0700

    Change LTS, add pari passu values to Rounds for tests, add stack.yaml.lock to gitignore.

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,7 @@ main :: IO ()
 main = do
   let inv = Investor "Angel"
   let i = Investment inv (Currency 2000000)
-  let r = Round "Seed" (fromGregorian 2000 1 1) (PreMoneyValuation 3000000) [i] Common (OptionPool (OptionPoolPercent 0.0) PreMoney) None
+  let r = Round "Seed" (fromGregorian 2000 1 1) (PreMoneyValuation 3000000) [i] Common (OptionPool (OptionPoolPercent 0.0) PreMoney) None False
   let c = Company [r] [Investment (Investor "Founder 1") (Shares 750000), Investment (Investor "Founder 2") (Shares 750000)]
   let result = pps (Calculation c Nothing) r
   print $ show result
--- test/Spec.hs
+++ test/Spec.hs
@@ -11,7 +11,7 @@ main = hspec $ do
     describe "CapTable" $ do
       it "calculates a simple pps" $ do
         let i = Investment angel (Currency 2000000)
-        let r = Round "Seed" (fromGregorian 2000 1 1) (PreMoneyValuation 3000000) [i] Common (OptionPool (OptionPoolPercent 0.0) PostMoney) None
+        let r = Round "Seed" (fromGregorian 2000 1 1) (PreMoneyValuation 3000000) [i] Common (OptionPool (OptionPoolPercent 0.0) PostMoney) None False
         let c = Company [r] [Investment (Investor "Founder 1") (Shares 750000), Investment (Investor "Founder 2") (Shares 750000)]
         let pps' = pps (Calculation c Nothing) r
         let ts' = ts (Calculation c Nothing) (Just r) 
@@ -19,13 +19,13 @@ main = hspec $ do
         ts' `shouldBe` 2500000
       it "calculates pps given a post money option pool" $ do
         let i = Investment angel (Currency 1000000)
-        let r = Round "Seed" (fromGregorian 2000 1 1) (PreMoneyValuation 4000000) [i] Common (OptionPool (OptionPoolPercent 0.15) PostMoney) None
+        let r = Round "Seed" (fromGregorian 2000 1 1) (PreMoneyValuation 4000000) [i] Common (OptionPool (OptionPoolPercent 0.15) PostMoney) None False
         let c = Company [r] [Investment (Investor "Founder 1") (Shares 2000000), Investment (Investor "Founder 2") (Shares 2000000)]
         let result = pps (Calculation c Nothing) r
         result `shouldBe` 1.00
       it "calculates the pre money option pool shares" $ do
         let i = Investment angel (Currency 1000000)
-        let r = Round "Seed" (fromGregorian 2000 1 1) (PreMoneyValuation 1000000) [i] Common (OptionPool (OptionPoolPercent 0.2) PreMoney) None
+        let r = Round "Seed" (fromGregorian 2000 1 1) (PreMoneyValuation 1000000) [i] Common (OptionPool (OptionPoolPercent 0.2) PreMoney) None False
         let c = Company [r] [Investment (Investor "Founder 1") (Shares 500000), Investment (Investor "Founder 2") (Shares 500000)]
         let pps' = pps (Calculation c Nothing) r
         let result = ops (Calculation c Nothing) (Just r)
